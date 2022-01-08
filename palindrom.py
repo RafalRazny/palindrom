69f8fd681676f27328d5148cdc5c0e121eabbab8
Rafał Raźny <rafal-razny@wp.pl>
2022-01-08 16:18:42 +0100

    Change function to one line palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,12 +1,4 @@
-list_of_letters = []
-revers_list_of_letters = []
 def palindrom(word):
-    for i in word:
-        list_of_letters.append(i)
-    revers_list_of_letters = list (reversed(list_of_letters))
-    if revers_list_of_letters == list_of_letters:
-        print("Tak, ten wyraz to jest palindrom!")
-    else:
-        print("Nie, ten wyraz to nie jest palindrom.")
+    return word == word[::-1]
 
-palindrom('kajak')
+print(palindrom('kajak'))
